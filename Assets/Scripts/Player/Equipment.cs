8e5835815da0771dbf3313cfabdf946b71fd4012
geuneda <whdrmsvjzl@naver.com>
2024-11-12 12:42:55 +0900

    feat: 동적으로 무기를 바꿀 수 있도록 코드 추가

--- Assets/Scripts/Player/Equipment.cs
+++ Assets/Scripts/Player/Equipment.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Item;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,6 +9,7 @@ public class Equipment : MonoBehaviour
     public Equip curEquip;
     public Transform equipParent;
 
+    private IWeaponStrategy strategy;
     private PlayerController controller;
     private PlayerCondition condition;
 
@@ -15,6 +17,25 @@ public class Equipment : MonoBehaviour
     {
         controller = GetComponent<PlayerController>();
         condition = GetComponent<PlayerCondition>();
+    }
+
+    // 무기변경시 호출하여 현재 무기 적용
+    public void SetWeapon(IWeaponStrategy newStrategy)
+    {
+        strategy = newStrategy;
+    }
+    
+    // 장비 변경 예제
+    public void SetWeapon()
+    {
+        // 활 장착
+        // new "" 부분은 무기 장착 방법에 따라 변경가능성
+        SetWeapon(new BowStrategy());
+        Attack(); // 화살 발사
+        SetWeapon(new GunStrategy());
+        Attack(); // 총알 발사
+        SetWeapon(new MagicStrategy());
+        Attack(); // 마법 공격
     }
 
     public void EquipNew(ItemData data)
@@ -31,7 +52,20 @@ public class Equipment : MonoBehaviour
             curEquip = null;
         }
     }
-
+    
+    // 전략 패턴을 적용한 새로운 공격 시스템
+    public void Attack()
+    {
+        if (strategy != null)
+        {
+            strategy.UseWeapon();
+        }
+        else
+        {
+            Debug.Log("No strategy selected");
+        }
+    }
+    
     public void OnAttackInput(InputAction.CallbackContext context)
     {
         if(context.phase == InputActionPhase.Performed && curEquip != null && controller.canLook)
